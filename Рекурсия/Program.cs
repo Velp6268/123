a39fefa8f9b07a79df1b90bf0b27228a6843d31a
Velp <bagor6268@gmail.com>
2022-09-29 16:03:48 +0300

    Test

---
+++ Рекурсия/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Recursia
+{
+    class Program
+    {
+       
+        static int Foo(int value)
+        {
+
+            if (value < 10)
+                return value;
+
+            return value % 10 + Foo(value / 10); 
+         
+
+           
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine(Foo(561));
+
+
+        }
+    }
+}
